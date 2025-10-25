a712335cdd97935ca53e11793aadff56105ee274
Ratbek Nurlanbekuulu <ratbek@protonmail.com>
2025-10-25 18:01:21 +0600

    fix: remove mainnet chain

--- packages/nextjs/services/web3/wagmiConfig.tsx
+++ packages/nextjs/services/web3/wagmiConfig.tsx
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { hardhat, mainnet, sepolia } from "wagmi/chains";
+import { hardhat, sepolia } from "wagmi/chains";
 
 export const wagmiConfig = getDefaultConfig({
-  appName: "Umay Agricultural Investment Platform",
+  appName: "Umay",
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "DEFAULT_PROJECT_ID",
-  chains: [hardhat, mainnet, sepolia],
+  chains: [hardhat, sepolia],
   ssr: true,
 });
